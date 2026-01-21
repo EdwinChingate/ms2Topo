c09a8d7a02b10bb4d97fed38fefd94c857fea4b7
eachingateb <eachingateb@unal.edu.co>
2026-01-21 06:57:06 -0500

    preparing functions for an upgrade in the second layer of clustering

--- Functions/CosineMatrix.py
+++ Functions/CosineMatrix.py
@@ -1,11 +1,12 @@
 import numpy as np
 from Cosine_2VecSpec import *
-def CosineMatrix(AlignedFragmentsMat,N_features):
-    CosineMat=np.zeros((N_features,N_features))
-    for feature_id1 in np.arange(N_features,dtype='int'):
-        for feature_id2 in np.arange(feature_id1,N_features,dtype='int'):
-            AlignedSpecMat=AlignedFragmentsMat[:,[0,feature_id1+1,feature_id2+1]]
-            Cosine=Cosine_2VecSpec(AlignedSpecMat=AlignedSpecMat)
-            CosineMat[feature_id1,feature_id2]=Cosine
-            CosineMat[feature_id2,feature_id1]=Cosine
+def CosineMatrix(AlignedFragmentsMat,
+                 N_features):
+    CosineMat = np.zeros((N_features,N_features))
+    for feature_id1 in np.arange(N_features-1,dtype='int'):
+        for feature_id2 in np.arange(feature_id1+1,N_features,dtype='int'):
+            AlignedSpecMat = AlignedFragmentsMat[:,[0,feature_id1+1,feature_id2+1]]
+            Cosine = Cosine_2VecSpec(AlignedSpecMat = AlignedSpecMat)
+            CosineMat[feature_id1,feature_id2] = Cosine
+            CosineMat[feature_id2,feature_id1] = Cosine
     return CosineMat
--- Functions/Retrieve_and_Join_ms2_for_feature.py
+++ Functions/Retrieve_and_Join_ms2_for_feature.py
@@ -8,7 +8,8 @@ def Retrieve_and_Join_ms2_for_feature(All_FeaturesTable,
                                       ms2_spec_id_col = 15,
                                       ms2Folder = 'ms2_spectra',
                                       ToAdd = 'mzML',
-                                      min_Int_Frac = 2):
+                                      min_Int_Frac = 2,
+                                      Norm2One = False):
     N_features = len(Feature_module)
     FeatureTable = All_FeaturesTable[Feature_module,:].copy()
     firstSpec = True
@@ -26,16 +27,21 @@ def Retrieve_and_Join_ms2_for_feature(All_FeaturesTable,
         if ExistSpectrum:
             ms2_spectrumDF = pd.read_csv(ms2_spectrumLoc,index_col = 0)
             ms2_spectrum = np.array(ms2_spectrumDF)      
+            if Norm2One:
+                TotalRelativeIntensity = np.sum(ms2_spectrum[:,9])
+                ms2_spectrum[:,9] = ms2_spectrum[:,9]/TotalRelativeIntensity
             N_peaks = len(ms2_spectrum[:,0])
             SpectrumLocVec = np.ones(N_peaks).reshape(-1,1)*feature_id
             ms2_spectrum = np.append(ms2_spectrum,SpectrumLocVec,axis = 1)
             if firstSpec:
-                All_ms2 = ms2_spectrum
+                All_ms2 = ms2_spectrum.copy()
                 firstSpec = False
             else:
                 All_ms2 = np.append(All_ms2,ms2_spectrum,axis = 0)   
     if len(All_ms2) == 0:
         return []                
+    if Norm2One:
+        min_Int_Frac = min_Int_Frac/TotalRelativeIntensity
     IntFrac_ms2_filter = All_ms2[:,9]>min_Int_Frac
     All_ms2 = All_ms2[IntFrac_ms2_filter,:]    
     return All_ms2
